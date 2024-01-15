3f9b970d7a7fb5e3819cf1545740b896c25da87a
Thit Lwin <thitlwincoder@gmail.com>
2024-01-15 11:47:17 +0630

    feat: initial commit

---
+++ lib/src/animated_item_menu.dart
@@ -0,0 +1,7 @@
+/// {@template animated_item_menu}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class AnimatedItemMenu {
+  /// {@macro animated_item_menu}
+  const AnimatedItemMenu();
+}
---
+++ test/src/animated_item_menu_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:animated_item_menu/animated_item_menu.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('AnimatedItemMenu', () {
+    test('can be instantiated', () {
+      expect(AnimatedItemMenu(), isNotNull);
+    });
+  });
+}
